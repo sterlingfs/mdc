ecd16823d4b86d2c682b41af2dbee11b0b050178
Brady Sterling <sterlingfsio@gmail.com>
2020-11-17 02:12:29 +0000

    button component

--- src/components/mdc-button/MDCButton.tsx
+++ src/components/mdc-button/MDCButton.tsx
@@ -6,6 +6,18 @@ import "@material/button/dist/mdc.button.css";
 
 export type MDCButtonProps = {
   title: string;
+  iconName: string;
+  type: keyof typeof buttonType;
+  trailingIcon: boolean;
+  disabled: boolean;
+
+  onClick: (ev: React.MouseEvent) => void;
+};
+
+const buttonType = {
+  normal: "mdc-button--outlined",
+  outlined: "mdc-button--outlined",
+  raised: "mdc-button--raised",
 };
 
 export function MDCButton(props: MDCButtonProps) {
@@ -16,11 +28,26 @@ export function MDCButton(props: MDCButtonProps) {
     el && new MDCRipple(el);
   });
 
+  const icon = (iconName: string) => (
+    <i className="material-icons mdc-button__icon" aria-hidden="true">
+      {iconName}
+    </i>
+  );
+
   return (
     <div className="mdc-touch-target-wrapper">
-      <button ref={rootRef} className="mdc-button mdc-button--touch">
+      <button
+        ref={rootRef}
+        className={`mdc-button mdc-button--touch ${
+          buttonType[props.type] || ""
+        }`}
+        disabled={props.disabled === true}
+        onClick={props.onClick}
+      >
         <div className="mdc-button__ripple"></div>
+        {props.iconName && !props.trailingIcon && icon(props.iconName)}
         <span className="mdc-button__label">{props.title}</span>
+        {props.iconName && props.trailingIcon && icon(props.iconName)}
         <div className="mdc-button__touch"></div>
       </button>
     </div>
