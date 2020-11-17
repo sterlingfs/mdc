bc0de7f176c99fd6f757c8f50f024c68fa30fa85
Brady Sterling <sterlingfsio@gmail.com>
2020-11-17 01:30:42 +0000

    test hooks

--- src/components/mdc-button/MDCButton.tsx
+++ src/components/mdc-button/MDCButton.tsx
@@ -4,19 +4,23 @@ import { MDCRipple } from "@material/ripple";
 
 import "@material/button/dist/mdc.button.css";
 
-export function MDCButton() {
-  const rootRef = React.createRef<HTMLDivElement>();
+export type MDCButtonProps = {
+  title: string;
+};
 
-  // useEffect(() => {
-  //   const el = rootRef.current;
-  //   el && new MDCRipple(el);
-  // });
+export function MDCButton(props: MDCButtonProps) {
+  const rootRef = React.createRef<HTMLButtonElement>();
+
+  useEffect(() => {
+    const el = rootRef.current;
+    el && new MDCRipple(el);
+  });
 
   return (
-    <div ref={rootRef} className="mdc-touch-target-wrapper">
-      <button className="mdc-button mdc-button--touch">
+    <div className="mdc-touch-target-wrapper">
+      <button ref={rootRef} className="mdc-button mdc-button--touch">
         <div className="mdc-button__ripple"></div>
-        <span className="mdc-button__label">My Accessible Button</span>
+        <span className="mdc-button__label">{props.title}</span>
         <div className="mdc-button__touch"></div>
       </button>
     </div>
